d15789752f4d834ff47ded8a51f7d472c86e2864
Christopher Nadler <christopher.nadler@gmail.com>
2026-02-15 10:33:29 +0100

    Rename file and change rights. Also deleta file log handler

--- dishwasher_controller.py
+++ dishwasher_controller.py
@@ -43,19 +43,17 @@ class DishwasherState(Enum):
 
 def setup_logging() -> logging.Logger:
     logger = logging.getLogger('DishwasherApp')
-    logger.setLevel(logging.DEBUG)
+    if Config.DEBUG:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
     
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    
-    file_handler = logging.FileHandler('dishwasher.log')
-    file_handler.setFormatter(formatter)
-    file_handler.setLevel(logging.INFO)
     
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
     console_handler.setLevel(logging.DEBUG)
     
-    logger.addHandler(file_handler)
     logger.addHandler(console_handler)
     
     return logger
